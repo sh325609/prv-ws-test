767fe5fbd2c3f12975f991da325c459fc6a9e99b
sh325609 <wangs_316@163.com>
2018-12-18 00:01:39 +0800

    test

--- ws-test-rocketmq/src/main/java/prv/ws/test/rocketmq/helloworld/RocketMQPushTestConsumer.java
+++ ws-test-rocketmq/src/main/java/prv/ws/test/rocketmq/helloworld/RocketMQPushTestConsumer.java
@@ -14,7 +14,7 @@ import java.util.List;
 /**
  *
  */
-public class RocketMQTestConsumer {
+public class RocketMQPushTestConsumer {
 
     public static void main(String[] args) throws InterruptedException, MQClientException {
 
